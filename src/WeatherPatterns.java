4e881ec65de539c7378ae51044307e657b9f6cbb
agastyabrahmbhatt <agastyabrahmbhatt@menloschool.org>
2025-01-13 21:40:26 -0800

    code

--- src/WeatherPatterns.java
+++ src/WeatherPatterns.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
+
 /**
  * The class WeatherPatterns finds the longest span of days in which
  * each day’s temperature is higher than on the previous day in that sequence.
  *
  * @author Zach Blick
- * @author YOUR NAME HERE
+ * @author Agastya Brahmbhatt
  */
 
 public class WeatherPatterns {
@@ -15,8 +17,30 @@ public class WeatherPatterns {
      * @return the longest run of days with increasing temperatures
      */
     public static int longestWarmingTrend(int[] temperatures) {
+        ArrayList<Integer> tempArr = new ArrayList<Integer>();
+        int longestRun = 0;
+        int currentRun = 0;
+        for(int i = 0; i < temperatures.length; i++){
+            currentRun = calcLongestRun(i, temperatures);
+            if(currentRun > longestRun){
+                longestRun = currentRun;
+            }
+        }
+        return longestRun + 1;
+    }
 
-
-        return 0;
+    private static int calcLongestRun(int startPos, int[] temperatures) {
+        if((startPos < 0) || (startPos > temperatures.length)){
+            return 0;
+        }
+        int currentTemp = temperatures[startPos];
+        int mRun = 0;
+        for(int i = startPos + 1; i < temperatures.length; i++){
+            if(temperatures[i] > currentTemp){
+                mRun++;
+                currentTemp = temperatures[i];
+            }
+        }
+        return mRun;
     }
 }
